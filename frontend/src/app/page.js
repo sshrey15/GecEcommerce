3ae13e3987e535938a0e50fea0c6b5b4fcc34ab3
shrey <shreysingh152004@gmail.com>
2023-11-30 15:21:15 +0530

    Update image URL in openModal function

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -188,7 +188,7 @@ export default function Home() {
                           console.log("Clicked");
                           console.log("hi");
                           openModal(
-                            `http://localhost:3001/${seller.images[0]}`
+                            `https://ecomproject1.onrender.com/${seller.images[0]}`
                           );
                         }}
                         class="align-middle select-none mr-10 font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
