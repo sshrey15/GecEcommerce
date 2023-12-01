3e1828d9b9f6af9c721aabd741bcc7806b7ec5ae
shrey <shreysingh152004@gmail.com>
2023-12-01 21:32:40 +0530

    Update footer logo and text

--- frontend/src/app/components/Footer.jsx
+++ frontend/src/app/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-grey">Flowbite</span>
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-grey">GecEcom</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
