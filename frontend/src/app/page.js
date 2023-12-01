20e7f74998cc9734ade316bf7ffacb94502d4d46
shrey <shreysingh152004@gmail.com>
2023-12-01 22:01:34 +0530

    Remove handleDelete function call

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -154,7 +154,7 @@ export default function Home() {
                   className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full"
                 >
                   <button
-                    onClick={() => handleDelete(seller._id)}
+                    // onClick={() => handleDelete(seller._id)}
                     class="absolute top-0 right-0 m-2 z-10 align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-red-600 text-white shadow-md hover:bg-red-700 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
                     type="button"
                   >
