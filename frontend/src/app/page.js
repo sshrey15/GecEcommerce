f4165a777dd498d49a5d3d9a524902004ad32434
shrey <shreysingh152004@gmail.com>
2023-12-25 18:38:49 +0530

    Refactor image button click event

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -195,11 +195,11 @@ export default function Home() {
                     <div class="flex space-x-2">
                       <button
                         onClick={() => {
-                          const imageUrl = `https://ecomproject1.onrender.com/${seller.images[0]}`;
-                          console.log(imageUrl);
-
+                          //shreu
+                          console.log("Clicked");
+                          console.log("hi");
                           openModal(
-                            imageUrl
+                            `https://ecomproject1.onrender.com/${seller.images[0]}`
                           );
                         }}
                         class="align-middle select-none mr-10 font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
