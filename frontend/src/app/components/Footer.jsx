701f7b93aaa12f37c00ca73d26499512952a9056
shrey <shreysingh152004@gmail.com>
2024-01-19 11:28:06 +0530

    Update copyright year in Footer component

--- frontend/src/app/components/Footer.jsx
+++ frontend/src/app/components/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023  All Rights Reserved.
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024  All Rights Reserved.
           </span>
          
       </div>
