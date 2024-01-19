8bda4a3d9b9bff15048ae7140a64a31b5e477197
shrey <shreysingh152004@gmail.com>
2024-01-19 14:15:35 +0530

    metda data change

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -4,16 +4,30 @@ import Authprovider from '../component/authprovider/Authprovider'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'GecBazaar - Your Online Marketplace',
   description: 'Explore the best products and deals on GecBazaar, your go-to online marketplace for quality and convenience.',
+  icons: {
+    icon: [
+
+      '/favicon.ico?v=4',
+    ],
+    apple: [
+      '/apple-touch-icon.png?v=4',
+    ],
+    shortcut: ['/apple-touch-icon.png?v=4'],
+
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
+
     <html lang="en">
+
       <body className={inter.className}>
         <Authprovider>
         <Navbar/>
